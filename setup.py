3408146c805373a8f3b5e49024b3cb28b3a47ffe
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-07-08 11:14:06 -0700

    fixed github url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='congressapi',
       version='0.1',
       description='Wrapper for ProPublica Congress API',
-      url='http://github.com/kjchavez/congress-api',
+      url='http://github.com/kjchavez/congressapi',
       author='Kevin Chavez',
       author_email='kevin.j.chavez@gmail.com',
       license='MIT',
