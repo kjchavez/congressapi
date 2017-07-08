cebd65dba3e58a742a4b2975d98c35a1c61842e8
Kevin Chavez <kjchavez@stanford.edu>
2017-07-08 12:15:55 -0700

    bumped version. should be more disciplined about this

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='congressapi',
-      version='0.2',
+      version='0.3',
       description='Wrapper for ProPublica Congress API',
       url='http://github.com/kjchavez/congressapi',
       author='Kevin Chavez',
